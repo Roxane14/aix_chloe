dd167f414a5ed503be9ed05b9e51fdc3dbd7b47a
Roxane Le Tensorer <roxane.letensorer@gmail.com>
2021-05-10 20:01:38 +0200

    maybe final push ? lol

--- script.js
+++ script.js
@@ -13,33 +13,92 @@ class Step {
 
 
 new Step(
-	"Cette charmante enfant est sans doute sous la douche. Cours la rejoindre !",
-	"Quel est le numero correspondant ?",
-	"quatre");
+	"Rendez-vous sur une place qui est la moiti\351 d'une demi-heure",
+	"Un dieu romain veille sur la fontaine. De qui s'agit-il ?",
+	"bacchus");
 
 new Step(
-	"Ou peut-etre a-t-elle voulu rechauffer un bon petit plat, car elle avait faim !",
-	"Quel est le numero correspondant ?",
-	"deux");
+	"Direction \340 pr\351sent le symbole d'une fonction primordiale : celle de gouverner la ville",
+	"Quels sont les couleurs du blason de la ville ?",
+	"jaune|bleu|rouge");
 
 new Step(
-	"Ce n'est toujours pas ca... Peut-etre qu'elle s'est assise confortablement dans son canape ?",
-	"Quel est le numero correspondant ?",
-	"sept");
+	"Ce lieu de culte semble \352tre vou\351 \340 quelqu'un qui est venu en aide, qui a secouru autrui.",
+	"Que tient entre ses bras le personnage central ?",
+	"enfant");
 
 new Step(
-	"Alors c'est surement qu'elle est en train de travailler sur son ordinateur !",
-	"Quel est le numero correspondant ?",
-	"huit");
+	"Mon premier est \351gal au nombre de petits cochons\
+	Mon second est plus pr\351cieux que l’argent\
+	Mon troisi\350me constitue les phrases",
+	"En quelle ann\351e un homme fut-il pendu en ces lieux ?",
+	"1524");
 
 new Step(
-	"Ou alors, elle est allee fermer a clef la porte d'entree...",
-	"Quel est le numero correspondant ?",
-	"trois");
+	"Un pr\351dicateur, faisant la morale aux autres, a bien m\351rit\351 sa propre place",
+	"Deux horloges bien diff\351rentes ne semblent pas tout \340 fait synchronis\351es... Combien d'ann\351es les s\351parent ?",
+	"8");
 
 new Step(
-	"Mais non voyons, c'est evident ! Elle est allee se blottir sous les draps et attend son prince charmant.",
-	"Pour lui prouver que tu as bien ete jusqu'au bout, dis-lui le mot magique : brouette",
+	"Le nom de cet h\364tel est un outil tr\350s pratique pour faire de la couture",
+	"Quelle \351tait la profession du commanditaire et architecte de ce lieu ?",
+	"collectionneur");
+
+new Step(
+	"Rajouter la plus forte carte \340 la fin d'un pr\351nom masculin nous donnera la prochaine \351tape",
+	"Combien de visages curieux vous observent en ces lieux ? ",
+	"13");
+
+new Step(
+	"L'\351poux d'une reine se situe sur cette fontaine",
+	"Quel fruit ce personnage embl\351matique a-t-il introduit dans la r\351gion ?",
+	"muscat");
+
+new Step(
+	"Si l’on veut aller se baigner, hors de question d'avoir de l'eau froide n'est-ce pas ?",
+	"Des gens ivres ont d\351truit un animal ici. Lequel ?",
+	"triton");
+
+new Step(
+	"Un peu moins de 10 armes de guerre meurtri\350res sont pr\351sentes ici",
+	"Cette fontaine est tr\350s belle, mais quelque chose d'autre l'est aussi... Quoi donc ?",
+	"epoque");
+
+new Step(
+	"Continuer tout droit jusqu'\340 la prochaine fontaine",
+	"Quels sont les trois animaux qui veillent sur ce lieu ?",
+	"poisson|lion|oie");
+
+new Step(
+	"Qu'obtient-on lorsque l'on croise la rue Espariat avec la rue de la Couronne ?",
+	"Scintillante, j'\351tends mes 12 bras dans toutes les directions. Qui suis-je ?",
+	"etoile");
+
+new Step(
+	"Mon premier est au nombre de sept dans une semaine\
+	Mon second permet la mastication\
+	Mon tout est un parc",
+	"De quelle ville venait l'homme en l'honneur duquel fut construit le boulodrome ?",
+	"paris");
+
+new Step(
+	"Il faut \340 pr\351sent se diriger vers l'h\364tel le plus proche du cin\351ma «Le Renoir»",
+	"Quelle ville italienne sera bient\364t à l'honneur en ces lieux ?",
+	"venise");
+
+new Step(
+	"Quatre mamiph\350res aquatiques, gracieux et bondissants, sont situ\351s \340 notre avant-derni\350re \351tape",
+	"Quel \351tait l'ancien nom de cette place ?",
+	"mazarino");
+
+new Step(
+	"Un petit pays europ\351en insulaire au large de l’Italie sera le point final de cette qu\352te",
+	"Quelle est la couleur pr\351dominante des portes par ici ?",
+	"rouge");
+
+new Step(
+	"F\351licitations, vous avez atteint la derni\350re étape !",
+	"Vous pouvez \340 pr\351sent rentrer chez vous avec le code suivant : 462",
 	"");
 
 
@@ -119,11 +178,20 @@ function displayGoodAnswer(index) {
 function myFunction(val) {
 	val = val.toLowerCase();
 	val = val.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-	if (val == steps[index].answer) {
+	if (checkAnswer(val, steps[index].answer)) {
 		index++;
 		sessionStorage.setItem("myStoredIndex", index);
 		displayGoodAnswer(index - 1);
 		displayStep(index);
 	}
+}
+
+function checkAnswer(val, answer) {
+	const words = answer.split('|');
+	for (i in words) {
+		if (!val.includes(words[i]))
+			return false;
+	}
+	return true;
 }
 
